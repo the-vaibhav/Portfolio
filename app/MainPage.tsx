c2f6f1ea85f885a558bc83f38df7e808b9adadbb
Vaibhav <vaibhavmhaske.here@gmail.com>
2024-03-19 16:13:49 +0530

    navbar

---
+++ app/MainPage.tsx
@@ -0,0 +1,7 @@
+import ThemeSwitch from "../components/ThemeSwitcher";
+
+export default function Main() {
+    return <main className="grow overflow-hidden px-6">
+        <ThemeSwitch className="w-40 h-40"></ThemeSwitch>
+    </main>
+}
