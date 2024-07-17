3bf88e6d7637bf5a7331a5a40e5fc49697e0a5c6
Vaibhav <vaibhavmhaske.here@gmail.com>
2024-07-17 14:15:37 +0530

    Resume page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,6 +1,15 @@
-export default function Resume() {
-    return <div>
-        this resume page
-    </div>
+import Header from "../../components/Header";
+import Navbar from "../../components/Navbar";
 
+export default function Resume() {
+    return <div className="max-w-7xl mx-auto">
+        <div className="min-h-screen flex">
+            <Navbar></Navbar>
+            <main className="grow overflow-hidden px-6 mb-10 pb-10">
+                <div className="w-full h-full max-w-[1072px] mx-auto flex flex-col relative">
+                    <Header></Header>
+                </div>
+            </main>
+        </div>
+    </div>
 }
---
+++ components/ResumeBox.tsx
@@ -0,0 +1,15 @@
+import { div } from "three/examples/jsm/nodes/Nodes.js"
+
+interface ResumeBox {
+    heading: string,
+    timeline: string,
+    subheading: string,
+    name: string,
+    description: string
+}
+
+export default function ResumeBox(props: ResumeBox) {
+    return (
+        <div></div>
+    )
+}
