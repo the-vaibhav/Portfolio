05af1e840389116f454bb61afabd4ecb001ceec2
Vaibhav <vaibhavmhaske.here@gmail.com>
2024-07-30 18:02:36 +0530

    more responsiveness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,10 +28,10 @@ const Nav = () => {
                                 <a className="w-full h-6 flex items-center justify-center relative after:absolute after:w-0.5 after:right-0 after:top-0 after:bottom-0 text-slate-400 hover:text-slate-500 dark:text-slate-500 dark:hover:text-slate-400" href="/projects"><span className="sr-only">Projects</span>
                                     <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20"><path fillOpacity=".16" d="M1 4h18v10H1z"></path><path d="M8 3h4V2H8v1ZM6 3V0h8v3h6v12H0V3h6ZM2 5v8h16V5H2Zm14 13v-2h2v4H2v-4h2v2h12Z"></path></svg></a>
                             </li>
-                            <li className='md:py-2'>
+                            {/* <li className='md:py-2'>
                                 <a className="w-full h-6 flex items-center justify-center relative after:absolute after:w-0.5 after:right-0 after:top-0 after:bottom-0 text-slate-400 hover:text-slate-500 dark:text-slate-500 dark:hover:text-slate-400" href="/resume"><span className="sr-only">Resume</span>
                                     <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="18" height="20"><path fillOpacity=".16" fillRule="nonzero" d="M1 5h16v14H1z"></path><path fillRule="nonzero" d="M2 6v12h14V6H2Zm16-2v16H0V4h18ZM2 2V0h14v2H2Z"></path></svg></a>
-                            </li>
+                            </li> */}
                             <li className='md:py-2'>
                                 <a className="w-full h-6 flex items-center justify-center relative after:absolute after:w-0.5 after:right-0 after:top-0 after:bottom-0 text-slate-400 hover:text-slate-500 dark:text-slate-500 dark:hover:text-slate-400" href="/guestbook"><span className="sr-only">Subscribe</span>
                                     <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="21" height="21"><path fillOpacity=".16" d="m13.4 18-3-7.4-7.4-3L19 2z"></path><path d="M13.331 15.169 17.37 3.63 5.831 7.669l5.337 2.163 2.163 5.337Zm-3.699-3.801L.17 7.53 20.63.37l-7.161 20.461-3.837-9.463Z"></path></svg></a>
