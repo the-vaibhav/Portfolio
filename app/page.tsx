9c2d9ea17c01fc51956ab3ad098cdd0e33552181
Vaibhav <vaibhavmhaske.here@gmail.com>
2024-08-18 14:19:38 +0530

    nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
+import { AnimatedTabs } from "../components/AnimatedTabs";
 import MainPage from "../components/MainPage";
 import Nav from "../components/Navbar";
-import NewNav from "../components/NewNav";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto min-h-screen lg:flex lg:justify-center lg:items-center">
       <div className="flex">
-        <Nav></Nav>
-        {/* <NewNav></NewNav> */}
+        {/* <Nav></Nav> */}
+        <AnimatedTabs />
         <MainPage></MainPage>
       </div>
     </div>
---
+++ components/AnimatedTabs.tsx
@@ -0,0 +1,50 @@
+import AnimatedBackground from '@/components/animated/animated-background';
+import { Home, PhoneCall, Settings, User } from 'lucide-react';
+
+export function AnimatedTabs() {
+    const TABS = [
+        {
+            label: 'Home',
+            icon: <Home className='h-5 w-5' />,
+        },
+        {
+            label: 'About',
+            icon: <User className='h-5 w-5' />,
+        },
+        {
+            label: 'Services',
+            icon: <Settings className='h-5 w-5' />,
+        },
+        {
+            label: 'Contact',
+            icon: <PhoneCall className='h-5 w-5' />,
+        },
+    ];
+
+    return (
+        <div className="fixed inset-x-0 bottom-0 z-[1000] mb-4 flex h-12 justify-center px-6">
+            <div className="flex items-center rounded-xl border border-zinc-950/10 backdrop-filter backdrop-blur-lg p-2 shadow-[rgba(142,140,152,0.2)_0px_0px_30px,rgba(219,216,224,0.2)_0px_0px_0px_1px] dark:shadow-[rgba(111,109,120,0.1)_0px_0px_30px,rgba(60,57,63,0.4)_0px_0px_0px_1px]">
+                <AnimatedBackground
+                    defaultValue={TABS[0].label}
+                    className='rounded-lg bg-zinc-100'
+                    transition={{
+                        type: 'spring',
+                        bounce: 0.2,
+                        duration: 0.5,
+                    }}
+                >
+                    {TABS.map((tab) => (
+                        <button
+                            key={tab.label}
+                            data-id={tab.label}
+                            type='button'
+                            className='inline-flex h-9 w-9 items-center justify-center text-zinc-500 transition-colors duration-100 focus-visible:outline-2 data-[checked=true]:text-zinc-950'
+                        >
+                            {tab.icon}
+                        </button>
+                    ))}
+                </AnimatedBackground>
+            </div>
+        </div>
+    );
+}
